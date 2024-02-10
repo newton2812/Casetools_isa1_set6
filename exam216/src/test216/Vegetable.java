b6a192eeab4035dbac1b7ee0f3a61ac89dac5a2b
newton2812 <newtonvital2819@gmail.com>
2024-02-10 10:37:23 +0530

    test

--- exam216/src/test216/Vegetable.java
+++ exam216/src/test216/Vegetable.java
@@ -3,5 +3,15 @@ package test216;
 public class Vegetable {
 	
 	String name;
+	Vegetable(String name)
+	{
+		this.name=name;
+	}
 
+	public static void main(String[] args) {
+		
+		Vegetable spinach=new Vegetable("Spinach");
+		System.out.println("Vegetable is " +spinach.name);
+	
+	}	
 }
